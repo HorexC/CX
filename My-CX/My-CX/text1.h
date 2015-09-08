0a955934760ec18226f16f8ccbaeb5bf0d06ec93
chenxiang <chenxiang@noah-fund.com>
2015-09-08 14:43:54 +0800

    test1

---
+++ My-CX/My-CX/text1.h
@@ -0,0 +1,13 @@
+//
+//  text1.h
+//  My-CX
+//
+//  Created by Horex on 15/9/8.
+//  Copyright (c) 2015年 mega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface text1 : UIButton
+
+@end
---
+++ My-CX/My-CX/text1.m
@@ -0,0 +1,13 @@
+//
+//  text1.m
+//  My-CX
+//
+//  Created by Horex on 15/9/8.
+//  Copyright (c) 2015年 mega. All rights reserved.
+//
+
+#import "text1.h"
+
+@implementation text1
+
+@end
